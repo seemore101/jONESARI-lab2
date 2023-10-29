e339b99db5a3c8b2debe4cb0514fcf8b39b8273d
seemore101 <82735897+seemore101@users.noreply.github.com>
2023-10-29 19:08:47 +0000

    Update ipc.c

--- ipc.c
+++ ipc.c
@@ -1,6 +1,12 @@
 // TODO: add the appropriate header files here
-
-
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/time.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <unistd.h>
+#include <string.h>
+#include "lab2.h"
 /**************************************************************
  *  ipc_create - creates a shared memory object called lab2 and
  *               returns a char pointer to the memory shared
@@ -16,11 +22,11 @@ char* ipc_create(int size){
     char* ptr;
 
     // TODO: create the shared memory object called lab2
-
+    fd = shm_open("lab2",O_CREAT | O_RDWR,0666);
     // TODO: configure the size of the shared memory object 
-
+    ftruncate(fd, size);
     // TODO: memory map the shared memory object */
-
+    ptr = mmap(0,size, PROT_READ | PROT_WRITE, MAP_SHARED, fd, 0);
     return ptr;
 }
 
