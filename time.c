740952857dc5e2b6bbe1e6256a7f583710473d30
seemore101 <82735897+seemore101@users.noreply.github.com>
2023-10-29 19:37:24 +0000

    Update time.c

--- time.c
+++ time.c
@@ -1,7 +1,13 @@
 #include <sys/time.h>
+#include <stdio.h>
 
 double elapsed_time(struct timeval* start_time, struct timeval* end_time){
     //TODO: return the difference between end_time and start_time.
-    
+    double result;
+    double start;
+    double end;
+    start = (start_time->tv_sec) + start_time->tv_usec;
+    end = (end_time->tv_sec) + end_time->tv_usec;
+    result = (end - start);
     return 0.0;
 }
