e6ab75b8b1e4f491f1e1f40c0e8d3d48b8b53efb
seemore101 <82735897+seemore101@users.noreply.github.com>
2023-10-29 21:59:55 +0000

    Create test.sh

---
+++ test.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+function cleanup()
+{
+    [ -e testFile ] && rm testFile
+}
+
+function error()
+{
+    echo -e "ERROR: $1"
+    cleanup
+    exit 1
+}
+
+make time &>> /dev/null
+
+if [ $? -ne 0 ]; then
+    error "ERROR: Makefile compilation has failed!" 
+fi
+
+[ -x time ] || error "ERROR: time not found or not executable."
+
+cleanup
+exit 0
