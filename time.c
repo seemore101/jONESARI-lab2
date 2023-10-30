567fcf8108621c93e980154a534ba3e5cbe06399
seemore101 <82735897+seemore101@users.noreply.github.com>
2023-10-30 02:25:47 +0000

    time.c

--- time.c
+++ time.c
@@ -3,11 +3,10 @@
 
 double elapsed_time(struct timeval* start_time, struct timeval* end_time){
     //TODO: return the difference between end_time and start_time.
-    double result;
-    double start;
-    double end;
-    start = (start_time->tv_sec) + start_time->tv_usec;
-    end = (end_time->tv_sec) + end_time->tv_usec;
-    result = (end - start);
-    return 0.0;
+  double elapsed_microseconds = (end_time->tv_sec - start_time->tv_sec) * 1000000.0 +
+                                  (end_time->tv_usec - start_time->tv_usec);
+
+    double elapsed_seconds = elapsed_microseconds / 1000000.0;
+
+    return elapsed_seconds;
 }
